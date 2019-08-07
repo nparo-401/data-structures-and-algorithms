3d38dc376500972920d91e316074ea4cf10454e6
Nicholas Paro <nichparo@amazon.com>
2019-08-07 14:39:37 -0700

    reordered variable calls

--- code401challenges/src/main/java/code401challenges/ArrayShift.java
+++ code401challenges/src/main/java/code401challenges/ArrayShift.java
@@ -3,18 +3,15 @@ package code401challenges;
 public class ArrayShift {
   public static int[] insertShiftArray(int[] arrToBeShifted, int valToBeAdded){
     int len = arrToBeShifted.length + 1;
-    int[] newArr = new int[len];
     int middle = len / 2;
+    int[] newArr = new int[len];
 
     for (int i = 0; i < len; i++) {
-      if (i < middle){
-        newArr[i] = arrToBeShifted[i];
-      } else if(i == middle){
-        newArr[i] = valToBeAdded;
-      } else {
-        newArr[i] = arrToBeShifted[i - 1];
-      }
+      if (i < middle) newArr[i] = arrToBeShifted[i];
+      else if(i == middle) newArr[i] = valToBeAdded;
+      else newArr[i] = arrToBeShifted[i - 1];
     }
+
     return newArr;
   }
 }
