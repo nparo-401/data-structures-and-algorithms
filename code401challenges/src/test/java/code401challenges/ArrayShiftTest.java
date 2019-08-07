9240dec13a27beb7968b5e28209902becc1f8305
Nicholas Paro <nichparo@amazon.com>
2019-08-07 10:35:16 -0700

    added tests for failure/edge case

--- code401challenges/src/test/java/code401challenges/ArrayShiftTest.java
+++ code401challenges/src/test/java/code401challenges/ArrayShiftTest.java
@@ -9,20 +9,28 @@ public class ArrayShiftTest {
   @Test
   public void testInsertShiftArray_desiredPass() {
     int[] testArr = new int[]{2,4,6,8};
+    int[] testArr2 = new int[]{4,8,15,23,42};
+    int[] testArr3 = new int[]{};
     int[] finalArr = new int[]{2,4,5,6,8};
+    int[] finalArr2 = new int[]{4,8,15,16,23,42};
+    int[] finalArr3 = new int[]{0};
+
     assertArrayEquals(
         "Shifted array does not equal expected array",
         finalArr,
-        ArrayShift.insertShiftArray(testArr, 5));
-  }
+        ArrayShift.insertShiftArray(testArr, 5)
+    );
 
-  @Test
-  public void testInsertShiftArray_desiredFail() {
-    int[] testArr2 = new int[]{4,8,15,23,42};
-    int[] finalArr2 = new int[]{4,8,15,23,42};
     assertArrayEquals(
-        "EXPECTED FAILURE: expected not equal to actual array",
+        "Shifted array does not equal expected array",
         finalArr2,
-        ArrayShift.insertShiftArray(testArr2, 16));
+        ArrayShift.insertShiftArray(testArr2, 16)
+    );
+
+    assertArrayEquals(
+        "Shifted array does not equal expected array",
+        finalArr3,
+        ArrayShift.insertShiftArray(testArr3, 0)
+    );
   }
 }
