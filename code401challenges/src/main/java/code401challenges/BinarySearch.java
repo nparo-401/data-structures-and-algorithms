0659b9392cbbe85dd441b87b0199ee9ff8d87a86
Nicholas Paro <nichparo@amazon.com>
2019-08-07 18:58:06 -0700

    method added and tests passed

--- code401challenges/src/main/java/code401challenges/BinarySearch.java
+++ code401challenges/src/main/java/code401challenges/BinarySearch.java
@@ -2,6 +2,16 @@ package code401challenges;
 
 public class BinarySearch {
   public static int binarySearch(int[] arr, int n) {
-    return 0;
+    int begin = 0;
+    int end = arr.length - 1;
+
+    while (begin < end || begin == end) {
+      int mid = (int)Math.ceil(((double)begin + (double)end) / 2);
+      if (arr[mid] == n) return mid;
+      else if (arr[mid] < n) begin = mid + 1;
+      else if (arr[mid] > n) end = mid - 1;
+    }
+
+    return -1;
   }
 }
--- code401challenges/src/test/java/code401challenges/BinarySearchTest.java
+++ code401challenges/src/test/java/code401challenges/BinarySearchTest.java
@@ -7,7 +7,42 @@ import static org.junit.Assert.*;
 public class BinarySearchTest {
 
   @Test
-  public void testBinarySearch() {
-    assertEquals("returns the wrong index", 0, BinarySearch.binarySearch(new int[]{1}, 1));
+  public void testBinarySearch_EvenLengthArray() {
+    int[] testArr = new int[]{1,2,3,4};
+    assertEquals(
+        "EvenLengthArray should return an index of 3",
+        3,
+        BinarySearch.binarySearch(testArr, 4)
+    );
+  }
+
+  @Test
+  public void testBinarySearch_OddLengthArray() {
+    int[] testArr = new int[]{1,2,3,4,5};
+    assertEquals(
+        "OddLengthArray should return an index of 1",
+        1,
+        BinarySearch.binarySearch(testArr, 2)
+    );
+  }
+
+  @Test
+  public void testBinarySearch_ZeroLengthArray() {
+    int[] testArr = new int[]{};
+    assertEquals(
+        "ZeroLengthArray should return -1",
+        -1,
+        BinarySearch.binarySearch(testArr, 1)
+    );
+  }
+
+  @Test
+  public void testBinarySearch_SearchKeyNotInArray() {
+    int[] testArr = new int[]{1,2,3,4};
+    assertEquals(
+        "SearchKeyNotInArray should return -1",
+        -1,
+        BinarySearch.binarySearch(testArr, 9)
+    );
   }
 }
