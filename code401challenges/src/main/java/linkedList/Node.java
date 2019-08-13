31931403fa38f6fea2cbaa2ef7746c13f9cac58b
Nicholas Paro <nichparo@amazon.com>
2019-08-12 18:12:56 -0700

    scaffolding and adding in working DoublyLinkedList

--- code401challenges/src/main/java/linkedList/Node.java
+++ code401challenges/src/main/java/linkedList/Node.java
@@ -1,11 +1,39 @@
 package linkedList;
 
 public class Node<T> {
-  public T data;
-  public Node<T> next;
+  private T data;
+  private Node<T> next;
+  private Node<T> previous;
 
-  public Node(T data, Node<T> next){
+  public Node(T data, Node<T> next) {
     this.data = data;
     this.next = next;
   }
+
+  public Node(T data, Node<T> next, Node<T> previous) {
+    this.data = data;
+    this.next = next;
+    this.previous = previous;
+  }
+
+  public T getValue() {
+    return this.data;
+  }
+
+  public void setNext(Node<T> node) {
+    this.next = node;
+  }
+
+  public Node<T> getNext() {
+    return this.next;
+  }
+
+  public void setPrevious(Node<T> node) {
+    this.previous = node;
+  }
+
+  public Node<T> getPrevious() {
+    return this.previous;
+  }
+
 }
